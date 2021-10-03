cda5a75ca0717b11650116cacf26190d8bf15de8
barao <marcobaraoneves@gmail.com>
2021-10-03 09:04:07 -0300

    fei pra desgraça, porém ta funfando

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from functools import partial
 from tkinter import *
+from tkinter.font import *
 from editor import Editor
 from utils import Utils
 from window_positioning import centered
 from image_system import ImageSystem
+from functools import partial
+from recents import Recents
 
 
 class App(Tk):
@@ -28,9 +31,55 @@ class App(Tk):
             )
         )
 
-        openFileButton = Button(self, text="Abrir um arquivo", command=self.open_file)
+        buttonFont = Font(size=14)
+        window = Canvas(self, bg="#000000", highlightthickness=0)
 
-        openFileButton.pack()
+        leftPanel = Canvas(window, bg="#000000", highlightthickness=0)
+        rightPanel = Canvas(window, bg="#000000", highlightthickness=0)
+
+
+        openFileButton = Button(
+            leftPanel,
+            text="Abrir um arquivo",
+            command=self.open_file,
+            borderwidth=0,
+            font=buttonFont
+        )
+        openFileButton.grid(row=0, column=0)
+
+        openSamples = Label(leftPanel, text="Abrir exemplos")
+        openSamples.grid(row=1, column=0)
+
+        samples = ["Camera", "Moedas", "Foguete", "Astronauta"]
+
+        on_click_sample = lambda sample: lambda e: self.open_sample(sample.lower())
+        on_click_file = lambda filepath: lambda e: self.open_file(filepath)
+        for index, sample in enumerate(samples):
+            label = Label(leftPanel, text=sample)
+            label.bind("<Button-1>", on_click_sample(sample))
+            label.grid(row=index+2, column=0)
+
+        leftPanel.grid(row=0, column=0, sticky="nsew")
+
+        openRecently = Label(rightPanel, text="Abertos recentemente")
+        openRecently.grid(row=0, column=0)
+
+        recents = Recents.read()
+
+        for index, recent in enumerate(recents):
+            label = Label(rightPanel, text=recent["name"])
+            if recent["type"] == "file":
+                label.bind("<Button-1>", on_click_file(recent["param"]))
+            else:
+                label.bind("<Button-1>", on_click_sample(recent["param"]))
+            label.grid(row=index+1, column=0)
+
+        rightPanel.grid(row=0, column=1, sticky="nsew")
+
+        window.grid_columnconfigure(0, weight=1, uniform="panels")
+        window.grid_columnconfigure(1, weight=1, uniform="panels")
+        window.grid_rowconfigure(0, weight=1)
+        window.pack(expand=1, fill=BOTH)
 
     def __open_editor_and_destroy(self, img):
         if img is not None:
@@ -39,12 +88,13 @@ class App(Tk):
             editor = Editor(img)
             editor.mainloop()
 
-    def open_file(self):
-        img = self.image_system.open_file()
+    def open_file(self, filepath=None):
+        img = self.image_system.open_file(filepath)
+
         self.__open_editor_and_destroy(img)
 
-    def open_camera(self):
-        img = self.image_system.open_sample("camera")
+    def open_sample(self, sample):
+        img = self.image_system.open_sample(sample)
 
         self.__open_editor_and_destroy(img)
 
